4dde1ebbb0dddd8ebdbfca851bf0813c8d0b1f6f
nesslei <botchedbunnies@gmail.com>
2022-11-07 13:51:30 -0500

    35hwh5w3h
    e4h5e5ha45

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 const DOMSelectors = {
+  Container: document.getElementById("container"),
   Album: document.getElementById("album"),
   Artist: document.getElementById("artist"),
   Picture: document.getElementById("picture"),
   Button: document.getElementById("btn"),
-  Reset: document.getElementById("reset"),
 };
 
-console.log(container.Reset);
+DOMSelectors.Button.addEventListener("click", function () {
+  let input = DomSelectorsinput.value;
+
+  console.log(input);
+
+  DOMSelectors.Container.insertAdjacentHTML(
+    "beforeend",
+    `
+      <p class ='album'> ${Album} </p>
+      <p class ='artist'> ${Artist} </p>
+      <p class ='picture'> ${Picture} </p>
+    `
+  );
+});
+
+DOMSelectors.container.insertAdjacentHTML;
