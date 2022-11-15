066f932ce327d0dcde4b9edc1151fd97f9e43d31
nesslei <botchedbunnies@gmail.com>
2022-11-15 13:59:04 -0500

    vwdavw
    wdvavda

--- main.js
+++ main.js
@@ -1,35 +1,32 @@
-//an object called “DOMSelectors” to hold your DOM Selectors
+//object called “DOMSelectors” to hold your DOM Selectors
 const DOMSelectors = {
-  container: document.querySelectorId("#container"),
+  container: document.querySelecto(`#container`),
   form: document.getElementById("form"),
 };
 //function that creates an object and calls the following functions
 DOMSelectors.form.addEventListener("submit", function (preventRefresh) {
   preventRefresh.preventDefault();
   let box = {};
-
-  box.artist = document.getElementById("artist"); 
-  box.picture = document.getElementById("picture"); 
-  console.log(box); 
+  box.name = document.getElementById("name");
+  box.artist = document.getElementById("artist");
+  box.picture = document.getElementById("picture");
+  console.log(box);
 
   //function that injects the newly created object into the document object model
   DOMSelectors.container.insertAdjacentHTML(
     "afterend",
     ` 
   <div class="card">
-  <img class="title" src=${box.Album}/> 
-  <p class="name" src=${box.artist}/> 
-  <p class="name" src=${box.picture}/> 
+  <img class="title" src=${box.name}/> 
+  <p class="name"> ${box.artist}/> 
+  <p class="name"> ${box.picture}/> 
+  <button class="remove"> remove </button>
   </div> 
   `
   );
 
   //a function that clears the input fields after injecting the object
-  DOMSelectors.foreach((container) => {
-    DOMSelectors.name = "";
-    DOMSelectors.artist = "";
-    DOMSelectors.pictre = "";
-  });
+  document.getElementById("name", "artist", "picture").value = "";
 
   //function to remove an object after they have been created
   let remove = document.querySelectorAll("#remove");
@@ -39,3 +36,5 @@ DOMSelectors.form.addEventListener("submit", function (preventRefresh) {
     });
   });
 });
+
+form();
